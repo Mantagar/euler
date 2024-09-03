332d80c9ea4b38ecc3f252ad0d66adb42e5f0779
Marcin Radzio <marcin.radzio@wp.pl>
2024-09-03 13:58:17 +0200

    Optimize prime generation utils

--- primes.py
+++ primes.py
@@ -1,57 +1,21 @@
 def primesSieve(n):
-    sieve = list(range(2, n+1))
-    primes = []
-    while len(sieve) > 0:
-        p = sieve[0]
-        primes.append(p)
-        nextSieve = []
-        multipliedSieve = [p*i for i in sieve]
-        sIndex = 1
-        msIndex = 0
-        while sIndex < len(sieve):
-            potentialP = sieve[sIndex]
-            while potentialP > multipliedSieve[msIndex]:
-                msIndex += 1
-            if potentialP != multipliedSieve[msIndex]:
-                nextSieve.append(potentialP)
-            sIndex += 1
-        sieve = nextSieve
-    return primes
-
-
-def primesNaive(n):
+    # by far the fastest as it doesn't have dividing overhead
+    # only odd numbers are considered
+    # prime p needs to be incremented by 2 p times to get its multiples
+    sieve = list(range(3, n+1, 2))
     primes = [2]
-    i = 3
-    while i <= n:
-        isPrime = True
-        for p in primes[1:]:
-            if i % p == 0:
-                isPrime = False
-                break
-            elif p*p >= i:
-                break
-        if isPrime:
-            primes.append(i)
-        i += 2
-    return primes
-
-def primesSkip3(n):
-    primes = [2,3]
-    skips = [2,4]
-    s = 0
-    i = 5
-    while i <= n:
-        isPrime = True
-        for p in primes[1:]:
-            if i % p == 0:
-                isPrime = False
-                break
-            elif p*p >= i:
-                break
-        if isPrime:
-            primes.append(i)
-        i += skips[s]
-        s = (s+1) % len(skips)
+    index = 0
+    jumpingIndex = 0
+    while index < len(sieve):
+        p = sieve[index]
+        primes.append(p)
+        jumpingIndex = index + p
+        while jumpingIndex < len(sieve):
+            sieve[jumpingIndex] = 0
+            jumpingIndex += p
+        index += 1
+        while index < len(sieve) and sieve[index] == 0:
+            index += 1
     return primes
 
 def generateSkips(startingPrimes):
@@ -76,13 +40,14 @@ def generateSkips(startingPrimes):
     return skips
 
 def primesSkip(n, startingPrimes=[2,3,5,7,11,13,17]):
-    primes = startingPrimes
+    # all the generated numbers will be coprime to startingPrimes
+    primes = []
     skips = generateSkips(startingPrimes)
     s = 0
     i = skips[-1] + 1
     while i <= n:
         isPrime = True
-        for p in primes[1:]:
+        for p in primes:
             if i % p == 0:
                 isPrime = False
                 break
@@ -92,7 +57,7 @@ def primesSkip(n, startingPrimes=[2,3,5,7,11,13,17]):
             primes.append(i)
         i += skips[s]
         s = (s+1) % len(skips)
-    return primes
+    return startingPrimes + primes
 
 def bSearch(n, data):
     # basic binary search
@@ -110,25 +75,13 @@ def bSearch(n, data):
 
 def testComplexity():
     import time
-    n = 1000000
+    n = 10**7
 
-    # terrible
-    #t = time.time()
-    #p = primesSieve(n)
-    #t = time.time() - t
-    #print("Sieve:", t, len(p))
+    t = time.time()
+    p = primesSieve(n)
+    t = time.time() - t
+    print("Sieve:", t, len(p))
 
-    #t = time.time()
-    #p = primesNaive(n)
-    #t = time.time() - t
-    #print("Naive:", t, len(p))
-
-    #t = time.time()
-    #p = primesSkip3(n)
-    #t = time.time() - t
-    #print("Skip 3:", t, len(p))
-
-    # it slows after 17
     startingPrimes = [2,3,5,7,11,13,17]
     for i in range(0,len(startingPrimes)):
         t = time.time()
@@ -137,11 +90,13 @@ def testComplexity():
         print("Skip {}:".format(startingPrimes[i]), t, len(p), startingPrimes[:i+1])
 
     """
-    Skip 2: 39.9777135848999 78498 [2]
-    Skip 3: 26.891463041305542 78498 [2, 3]
-    Skip 5: 21.625972509384155 78498 [2, 3, 5]
-    Skip 7: 18.59758973121643 78498 [2, 3, 5, 7]
-    Skip 11: 16.970937967300415 78498 [2, 3, 5, 7, 11]
-    Skip 13: 16.122613191604614 78498 [2, 3, 5, 7, 11, 13]
-    Skip 17: 15.286912202835083 78498 [2, 3, 5, 7, 11, 13, 17]
+n = 10**7
+Sieve: 1.9706051349639893 664579              <------ by far the best
+Skip 2: 15.992695569992065 664579 [2]         <------ no gain for connsecutive skips (int limits?)
+Skip 3: 15.685003757476807 664579 [2, 3]
+Skip 5: 15.313693761825562 664579 [2, 3, 5]
+Skip 7: 15.313390254974365 664579 [2, 3, 5, 7]
+Skip 11: 15.109517812728882 664579 [2, 3, 5, 7, 11]
+Skip 13: 14.996946096420288 664579 [2, 3, 5, 7, 11, 13]
+Skip 17: 15.073673009872437 664579 [2, 3, 5, 7, 11, 13, 17]
     """
